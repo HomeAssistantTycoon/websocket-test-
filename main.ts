202add533a091da9984a3c6c18669e3400356b9f
HomeAssistantTycoon <jgsimon@icloud.com>
2025-07-29 14:23:42 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-let socket = sockets.connect("wss://echo.websocket.events") // Free test server
-socket.onMessage(msg => {
-    game.splash("Received: " + msg)
-})
-socket.send("hello from MakeCode!")
 
