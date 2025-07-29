092eeaf85d8b1ce075f7c5be1da9b573523bc6c5
HomeAssistantTycoon <jgsimon@icloud.com>
2025-07-29 14:35:18 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
+let socket = sockets.connect("https://websocket-test-t9ah.onrender.com")
+
+socket.onMessage((msg) => {
+    game.splash("Received: " + msg)
+})
+
+game.splash("Sending hello...")
+socket.send("hello from MakeCode!")
 
